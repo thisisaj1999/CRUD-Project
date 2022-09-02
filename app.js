e37818d4f825c6f128e9e5ad415ef6e3e84d1714
thisisaj1999 <atuljoshi94170@gmail.com>
2022-09-02 20:59:08 +0530

    Update and Delete method created

--- app.js
+++ app.js
@@ -34,29 +34,62 @@ app.post('/', (req, res) => {
 app.get('/list', async (req, res) => {
   User.find().exec((err, users) => {
     res.render('list', {
-      users: users,
+      usersList: users,
     });
   });
 });
 
-app.delete('/delete/:id', (req, res) => {
-  User.findByIdAndRemove(req.params.id, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
+app.get('/delete/:id', (req, res) => {
+  User.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.redirect('/list');
       console.log(`User deleted successfully...`);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+// get route update user
+app.get('/edit/:id', (req, res) => {
+  User.findById(req.params.id, (err, user) => {
+    if (err) {
+      res.redirect('/list');
+    } else {
+      if (user == null) {
+        res.redirect('/list');
+      } else {
+        res.render('edit', {
+          userUpdate: user,
+        });
+      }
     }
   });
 });
 
-// app.delete('/reviews/:id', function (req, res) {
-//   console.log("DELETE review")
-//   Review.findByIdAndRemove(req.params.id).then((review) => {
-//     res.redirect('/');
-//   }).catch((err) => {
-//     console.log(err.message);
-//   })
-// })
+// post route update user
+app.post('/update/:id', (req, res) => {
+  User.findByIdAndUpdate(
+    req.params.id,
+    {
+      fname: req.body.fname,
+      lname: req.body.lname,
+      email: req.body.email,
+      contact: req.body.contact,
+      address: req.body.address,
+      city: req.body.city,
+      gender: req.body.gender,
+    },
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('User updated successfully...');
+        res.redirect('/list');
+      }
+    }
+  );
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
